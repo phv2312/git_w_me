d5c7450b47de01af8124faa51b6096cdf1e8091a
kan_cin <57749972+phv2312@users.noreply.github.com>
2022-10-23 14:06:39 +0700

    Update intro_nancy.py

--- intros/intro_nancy.py
+++ intros/intro_nancy.py
@@ -2,5 +2,5 @@ def introduce(msg: str):
     print (msg)
 
 if __name__ == '__main__':
-    msg = "Hello world, this is Nancy from Cinnamon"
+    msg = "Hello world, this is Nancy - Flax QAL Lead, from Cinnamon"
     introduce(msg)
