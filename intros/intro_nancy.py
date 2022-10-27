d4cdfae11991011934dae983498b2d68c90e7a8e
phv2312 <kat87yb@gmail.com>
2022-10-27 10:17:10 +0700

    update intro_nancy.py

--- intros/intro_nancy.py
+++ intros/intro_nancy.py
@@ -1,5 +1,5 @@
 def introduce(msg: str):
-    print (msg)
+    print ("This is the rebase version: {msg}")
 
 if __name__ == '__main__':
     msg = "Hello world, this is Nancy - Flax QAL Lead, from Cinnamon"
